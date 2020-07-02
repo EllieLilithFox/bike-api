166a761a4c702ae46be787f862cbe477eb768f0a
Deryck Jackson <44139628+DeryckJackson@users.noreply.github.com>
2020-07-02 11:16:20 -0700

    add UI function call for bike search
    co-authored by: Evgeniya Chernaya <evgenya.chernaya@gmail.com>

--- src/main.js
+++ src/main.js
@@ -2,8 +2,27 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import $ from "jquery";
+import { getStolenBikes } from "./bike-service.js"
 
 $(document).ready(function() {
+  $("form").submit(function(event){
+    event.preventDefault();
+    let city = $("#city").val();
+    let state = $("#state").val();
+    let zip = $("#zip").val();
+    let address;
+    if(zip) {
+      address = zip;
+    } else {
+      address = `${city}, ${state}`;
+    }
 
+    getStolenBikes(address).then((data) => {
+      console.log(data.bikes[0].title);
+      data.bikes.forEach(function(element){
+        $(".output").append(`<p>${element.title}</p>`);
+      });
+    });
+  });
 });
 
