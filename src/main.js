313bee51ff83cfd362e24af071a9e9eb99d95b99
Deryck Jackson <44139628+DeryckJackson@users.noreply.github.com>
2020-07-02 13:55:58 -0700

    add css styles and add bikes to bottom of screen
    co-authored by: Evgeniya Chernaya <evgenya.chernaya@gmail.com>

--- src/main.js
+++ src/main.js
@@ -4,9 +4,9 @@ import './styles.css';
 import $ from "jquery";
 import { getStolenBikes } from "./bike-service.js";
 
-function pastWeek() {
+function pastWeek(days) {
   let today = new Date();
-  let result = new Date().setDate(today.getDate() - 7);
+  let result = new Date().setDate(today.getDate() - days);
   return Math.floor(result/1000);
 }
 
@@ -16,6 +16,7 @@ $(document).ready(function() {
     let city = $("#city").val();
     let state = $("#state").val();
     let zip = $("#zip").val();
+    let days = parseInt($("#days").val());
     let address;
     if(zip) {
       address = zip;
@@ -24,14 +25,23 @@ $(document).ready(function() {
     }
 
     getStolenBikes(address).then((data) => {
-      console.log(data.bikes[0].title);
-      console.log(Date.now(), pastWeek());
       data.bikes.forEach(function(element){
-        if(element.date_stolen > pastWeek()) {
-          $(".output").append(`<p>${element.title} - ${element.url}</p>`);
+        if(element.date_stolen > pastWeek(days) && element.thumb) {
+          $(".output").append(`<div class="flexbox">
+          <p>${element.title}</p>
+          <img src="${element.thumb}">
+          <p>Frame colors: ${element.frame_colors.join(", ")}</p>
+          <p>Serial: ${element.serial}</p>
+          <p>URL for bike owner: <a href="${element.url}">Link</a></p>
+          <p>Stolen Location: ${element.stolen_location}</p>
+          </div>`);
         }
       });
     });
+
+    $("#city").val("");
+    $("#state").val("");
+    $("#zip").val("");
   });
 });
 
