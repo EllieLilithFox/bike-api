4844680390d3f9da588a611bbe935e53e4fcd60c
Deryck Jackson <44139628+DeryckJackson@users.noreply.github.com>
2020-07-02 11:45:31 -0700

    add search date filter
    co-authored by: Evgeniya Chernaya <evgenya.chernaya@gmail.com>

--- src/main.js
+++ src/main.js
@@ -2,7 +2,13 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import $ from "jquery";
-import { getStolenBikes } from "./bike-service.js"
+import { getStolenBikes } from "./bike-service.js";
+
+function pastWeek() {
+  let today = new Date();
+  let result = new Date().setDate(today.getDate() - 7);
+  return Math.floor(result/1000);
+}
 
 $(document).ready(function() {
   $("form").submit(function(event){
@@ -19,8 +25,11 @@ $(document).ready(function() {
 
     getStolenBikes(address).then((data) => {
       console.log(data.bikes[0].title);
+      console.log(Date.now(), pastWeek());
       data.bikes.forEach(function(element){
-        $(".output").append(`<p>${element.title}</p>`);
+        if(element.date_stolen > pastWeek()) {
+          $(".output").append(`<p>${element.title} - ${element.url}</p>`);
+        }
       });
     });
   });
