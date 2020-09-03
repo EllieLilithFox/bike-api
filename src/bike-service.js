44f1d9543fa0956b53d1b0a45224befe68533814
Deryck Jackson <44139628+DeryckJackson@users.noreply.github.com>
2020-09-03 13:29:26 -0700

    gh-pages

--- src/bike-service.js
+++
@@ -1,5 +0,0 @@
-export async function getStolenBikes(location) {
-  let response = await fetch(`https://bikeindex.org/api/v3/search?page=1&per_page=25&location=${location}&distance=10&stolenness=proximity`);
-  let data = await response.json();
-  return data;
-}
